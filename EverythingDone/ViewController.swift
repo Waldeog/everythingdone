698774fc30628c3dda4b9022f57422c4399a5da0
Valtteri Ranta <valtteriranta@valtteris-mbp.home>
2016-11-18 18:55:57 +0200

    Initial Commit

---
+++ EverythingDone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EverythingDone
+//
+//  Created by Valtteri Ranta on 18/11/2016.
+//  Copyright © 2016 Valtteri Ranta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
